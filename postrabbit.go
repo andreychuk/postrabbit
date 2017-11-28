f03011b11238d799d54ee92c59d2c99bddd76bb4
vanya andreychuk <vandreychyk@gmail.com>
2017-11-28 12:41:42 +0200

    - fix typo

--- postrabbit.go
+++ postrabbit.go
@@ -8,8 +8,8 @@ type Config struct {
 	CHANNEL_LIST          string `env:"CHANNEL_LIST"`
 	POSTGRES_URL          string `env:"POSTGRES_URL"`
 	RABBITMQ_URL          string `env:"RABBITMQ_URL"`
-	DEFAULT_EXCHANGE_NAME string `end:"DEFAULT_EXCHANGE_NAME" envDefault:"default"`
-	DELAY_EXCHANGE_NAME   string `end:"DELAY_EXCHANGE_NAME" envDefault:"delay"`
+	DEFAULT_EXCHANGE_NAME string `env:"DEFAULT_EXCHANGE_NAME" envDefault:"default"`
+	DELAY_EXCHANGE_NAME   string `env:"DELAY_EXCHANGE_NAME" envDefault:"delay"`
 }
 
 func main() {
